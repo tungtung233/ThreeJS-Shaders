51a78f1111e7226d5bbf43a7b7f0ccface08ccb8
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-14 18:04:14 -0500

    understanding glsl types - basic data types

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,3 +1,22 @@
+// cannot console.log in glsl
+// need semi-colon at the end of every line
+
+// glsl is a typed language - have to declare every variable's type
+
+//have to decare it's type
+//float = has decimal point
+float a = 1.21212;
+
+//int = whole number
+int b = 2;
+
+// can't just add a float and int together
+float c = a + float(b);
+
+bool foo = true;
+
+
+
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
