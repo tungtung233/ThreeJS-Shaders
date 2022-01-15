db5246dc4e7497848b401373f9c335c2d1653af4
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-15 03:22:01 -0500

    understanding fragment shader fundamentals

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,3 +1,9 @@
+// the fragment shader's purpose is to color each visible fragement (pixel) of the geometry.
+
+// the same fragment shader will be used for every visible fragment of the geometry
+
+// the fragment shader is executed after the vertex shader
+
 precision mediump float;
 
 void main() {
