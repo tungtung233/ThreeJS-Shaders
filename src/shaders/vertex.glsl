f3d0a584223c4eb378f3226aad822e94481a1c07
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-15 02:50:18 -0500

    understanding glsl types - vec2, vec3, vec4, functions

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -15,6 +15,40 @@ float c = a + float(b);
 
 bool foo = true;
 
+vec2 coordinatesA = vec2(1.0, -2);
+
+// you can't create an empty vector
+// error -> 'vec2 coordinates = vec2();
+
+// but you can put in just 1 value and it will apply it to all slots
+vec3 coordinatesB = vec3(1.0);
+
+vec3 coordinatesC1 = vec3(coordinatesA, 0.1); // => (1.0, -2, 0.1)
+
+// vectors args => (x, y, z, w) or it can be (r, g, b, a)
+vec3 coordinatesC2 = vec3(coordinatesA.x, coordinatesA.y, 0.1); // => (1.0, -2, 0.1)
+vec3 coordinatesC3 = vec3(coordinatesA.r, coordinatesA.g, 0.1); // => (1.0, -2, 0.1)
+
+// this is called swizzling -> but I find it kind of hard/confusing to follow
+vec2 coordinatesD1 = coordinatesC2.xy; // => (1.0, -2)
+vec2 coordinatesD2 = coordinatesC2.yx; // => (-2, 1.0)
+vec2 coordinatesD3 = coordinatesC2.xz; // => (1.0, 0.1)
+
+vec4 coordinatesE = vec4(1.0, 2.0, 3.0, 4.0);
+
+// A function must start with the type of value that will be returned
+float loremIpsum() {
+  float varA = 1.0;
+  float varB = 2.1;
+
+  return varA + varB;
+}
+
+// If the function isn't supposed to return anythying, set the type to void'
+void exampleFunction() {
+  float varA = 1.0;
+  float varB = 2.1;
+}
 
 
 uniform mat4 projectionMatrix;
